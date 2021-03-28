0378009bc3806e8af8e31c52fe948a3c9be8b138
July Sudarenko <july@sudarenko.ru>
2021-03-28 12:06:05 +0300

    refactoring

--- Assets/Scripts/GenerateLevel/Generator/PlatformTileManager.cs
+++ Assets/Scripts/GenerateLevel/Generator/PlatformTileManager.cs
@@ -8,52 +8,50 @@ namespace Level
         private readonly int _waterLevel;
         private readonly int _maxJumpPlace = 4;
         private readonly int _cleanSize = 2;
-        private int _obstacleCounter = 0;
+        private int _obstacleCounter;
 
         private int[,] _map;
 
         public PlatformTileManager(int widthMap, int heightMap, int waterLevel) : base(widthMap, heightMap)
         {
-            _waterLevel = waterLevel;
+            _waterLevel = waterLevel - 1;
         }
 
         public override int[,] ReplaceTiles(int[,] map)
         {
             _map = map;
+            _obstacleCounter = 0;
             for (int x = 1; x < WidthMap - 1; x++)
             {
-                for (int y = _waterLevel - 1; y < _waterLevel; y++)
+                if (_map[x, _waterLevel] == (int) Water && _map[x + 1, _waterLevel] == (int) Water)
                 {
-                    if (_map[x, y] == (int) Water && _map[x + 1, y] == (int) Water)
+                    if (_map[x - 1, _waterLevel] == (int) Dirt || _map[x - 1, _waterLevel] == (int) Grass)
                     {
-                        if (_map[x - 1, y] == (int) Dirt || _map[x - 1, y] == (int) Grass)
+                        AddPlatform(x);
+                    }
+                    else
+                    {
+                        _obstacleCounter++;
+                        if (_obstacleCounter > _maxJumpPlace)
                         {
-                            InstallPlatform(x, y);
-                        }
-                        else
-                        {
-                            _obstacleCounter++;
-                            if (_obstacleCounter > _maxJumpPlace)
-                            {
-                                InstallPlatform(x, y);
-                            }
+                            AddPlatform(x);
                         }
                     }
-
-                    if (_map[x, y] == (int) Water && _map[x + 1, y] != (int) Water)
-                    {
-                        _obstacleCounter = 0;
-                    }
+                }
+                
+                if (_map[x, _waterLevel] == (int) Water && _map[x + 1, _waterLevel] != (int) Water)
+                {
+                    _obstacleCounter = 0;
                 }
             }
 
             return _map;
         }
 
-        private void InstallPlatform(int x, int y)
+        private void AddPlatform(int x)
         {
             int newX = x + Random.Range(0, 2);
-            int newY = y + Random.Range(3, 5);
+            int newY = _waterLevel + Random.Range(3, 5);
             var size = Random.Range(2, 5);
 
             _obstacleCounter -= size;
--- Assets/Scripts/GenerateLevel/Generator/WaterTileManager.cs
+++ Assets/Scripts/GenerateLevel/Generator/WaterTileManager.cs
@@ -3,23 +3,20 @@
 namespace Level
 {
     internal class WaterTileManager : TileManager
-     {
+    {
         private readonly int _waterLevel;
 
         public WaterTileManager(int widthMap, int heightMap, int waterLevel) : base(widthMap, heightMap)
         {
-            _waterLevel = waterLevel;
+            _waterLevel = waterLevel - 1;
         }
-        
+
         public override int[,] ReplaceTiles(int[,] map)
         {
             for (int x = 1; x < WidthMap - 1; x++)
             {
-                for (int y = _waterLevel - 1; y < _waterLevel; y++)
-                {
-                    if (map[x, y] == (int) Empty)
-                        map[x, y] = (int) Water;
-                }
+                if (map[x, _waterLevel] == (int) Empty)
+                    map[x, _waterLevel] = (int) Water;
             }
 
             return map;
